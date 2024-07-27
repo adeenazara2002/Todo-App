e8c64276276525d7c78b2da3dc88714776c22812
adeenazara2002 <adinazara2001@gmail.com>
2024-07-28 00:01:02 +0500

    todo

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -9,26 +9,61 @@ class SplashScreen extends StatelessWidget {
       body: Container(
         height: 700,
         width: double.infinity,
-        color: Color.fromRGBO(84, 90, 167, 0.886), 
+        color: Color.fromRGBO(84, 90, 167, 0.886),
         child: Scrollbar(
           child: SingleChildScrollView(
             child: Column(
               children: [
                 Padding(padding: EdgeInsets.only(top: 30)),
-                Text('Manage your' , style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1), 
-                fontWeight: FontWeight.w500, fontSize: 28)),
-
-                Text('Daily TO DO' , style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1), 
-                fontWeight: FontWeight.w500, fontSize: 28)),
-
+                Text('Manage your',
+                    style: TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 1),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 28)),
+                Text('Daily TO DO',
+                    style: TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 1),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 28)),
                 Image.asset('./assets/images/main_image.png'),
-
                 Container(
-                  height: 20,
-                  width: 50,
-              
+                  child: Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(top: 40, left: 70)),
+
+                      // container 1
+
+                      Container(
+                        height: 20,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 199, 39, 1),
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Color.fromRGBO(255, 199, 39, 1),
+                          ),
+                        ),
+                      ),
+
+                      // container 2
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        // padding: EdgeInsets.only(left: 60),
+                        height: 20,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(252, 216, 115, 1),
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Color.fromRGBO(252, 216, 115, 1),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 )
-                
               ],
             ),
           ),
